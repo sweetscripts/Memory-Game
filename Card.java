f557e1141c88d4e12836612eaaa5a539cacf76f8
sweetscripts <130302376+sweetscripts@users.noreply.github.com>
2023-12-01 09:03:17 -0500

    update comments Card.java

--- Card.java
+++ Card.java
@@ -2,6 +2,7 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.ImageObserver;
 
+//// Card class extends GameObject to represent individual cards in the game
 public class Card extends GameObject {
 
 	public boolean flipped = false;
@@ -15,21 +16,21 @@ public class Card extends GameObject {
 	public void update() {
 	}
 
-	// setting image
+	//Method to set the image of the card
 	public void setImage(Image i) {
 		super.img = i;
 	}
-
+	// Method to get the image of the card
 	public Image getImage() {
 		return super.img;
 	}
 
-	// setting fliped card
+	//Method to flip the card
 	public boolean flip() {
 		flipped = !flipped;
 		return flipped;
 	}
-
+	//// Drawing the card on the canvas
 	@Override
 	public void draw(Graphics g, ImageObserver o) {
 		if (super.img != null) {
